986b1e40a78b46a9a73e6c903a989557e4dea2e0
Maxim <enginer385@gmail.com>
2022-01-13 14:28:57 +1100

    Added role dependant filters to get orders endpoint

--- backend/app/orders.py
+++ backend/app/orders.py
@@ -15,6 +15,14 @@ from . import schemas
 
 @app.get(
     "/api/v1/order", response_model=List[schemas.Order], tags=["orders"]
-)  # TODO:Filter orders in responsibility with user role
+)
 async def order(user: models.User = Depends(manager), db: Session = Depends(get_db)):
-    return db.query(models.OrderWithUsers).all()
+    if user.role == models.UserType.manager:
+        return db.query(models.OrderWithUsers).filter(models.OrderWithUsers.manager_id == user.id).all()
+    elif user.role == models.UserType.chef:
+        return db.query(models.OrderWithUsers).all()
+    else:
+        return db.query(models.OrderWithUsers).filter(models.OrderWithUsers.customer_id == user.id).all
+
+
+
