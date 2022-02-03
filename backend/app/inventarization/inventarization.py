2f5a398438a7e66859559018198ddb66d1dbed96
Maxim <enginer385@gmail.com>
2022-02-03 23:45:20 +1000

    added getter of all storage items status

--- backend/app/inventarization/inventarization.py
+++ backend/app/inventarization/inventarization.py
@@ -17,19 +17,22 @@ def get_all_items_status(user: models.User = Depends(manager), db: Session = Dep
     for cloth_article in db.query(models.ClothStorage.article).distinct():
         cloth: models.Cloth = db.query(models.Cloth).filter(models.Cloth.article == cloth_article).one()
         all_clothes: List[models.ClothStorage] = db.query(models.ClothStorage).filter(models.ClothStorage.article == cloth_article).all()
-        area = 0.0
         for cloth_storage in all_clothes:
-            area += float(cloth_storage.length*cloth.width)
+            area = float(cloth_storage.length*cloth.width)
+            answer.append({
+                "type": "cloth_batch",
+                "amount": area,
+                "cloth": cloth
+            })
         all_patches: List[models.Patch] = db.query(models.Patch).filter(models.Patch.article == cloth_article).all()
 
         for patch in all_patches:
-            area += float(patch.width*patch.length)
-
-        answer.append({
-            "type": "cloth",
-            "amount": area,
-            "cloth": cloth
-        })
+            area = float(patch.width*patch.length)
+            answer.append({
+                "type": "cloth_patch",
+                "amount": area,
+                "cloth": cloth
+            })
 
     for accessory in db.query(models.AccessoriesStorage).all():
         accessory_row: models.Accessory = db.query(models.Accessory).filter(models.Accessory.article == accessory.article).one()
