0d54c8c2aa85f68b876c3d8cf13a173b7196f1f0
Maxim <enginer385@gmail.com>
2022-02-03 14:04:21 +1000

    Added decommission in kilogramms for accessories

--- backend/app/inventarization/decommissions.py
+++ backend/app/inventarization/decommissions.py
@@ -18,6 +18,8 @@ async def cloth_decommission(
     user: models.User = Depends(manager),
     db: Session = Depends(get_db),
 ):
+    if user.type not in [models.UserType.chef, models.UserType.storage_manager]:
+        raise exceptions.InsufficientPrivileges
     batch: Optional[models.ClothStorage] = (
         db.query(models.ClothStorage)
         .filter(
@@ -57,11 +59,63 @@ async def accessory_decommission(
     user: models.User = Depends(manager),
     db: Session = Depends(get_db),
 ):
+    if user.type not in [models.UserType.chef, models.UserType.storage_manager]:
+        raise exceptions.InsufficientPrivileges
     batch: Optional[models.AccessoriesStorage] = (
         db.query(models.AccessoriesStorage)
         .filter(models.AccessoriesStorage.article == article)
         .one_or_none()
     )
+    if batch is None:
+        raise exceptions.InvalidClothStorage
+
+    if batch.amount < quantity:
+        raise exceptions.InsufficientAccessoryCount
+
+    batch.amount -= quantity
+    if batch.amount == 0:
+        db.delete(batch)
+
+    new_decommission = models.AccessoryChanges()
+    new_decommission.amount = quantity
+    new_decommission.accessory_article = article
+    new_decommission.is_income = False
+
+    db.add(new_decommission)
+    db.commit()
+    db.flush()
+
+    return JSONResponse(
+        status_code=200, content={"description": "Decommissioned successfully"}
+    )
+
+
+@app.patch("/api/v1/accessory_in_kg/{article}", tags=["storage"])
+def accessory_in_kg_decommission(article: int, amount: float, user: models.User = Depends(manager), db: Session = Depends(get_db)):
+    if user.type not in [models.UserType.chef, models.UserType.storage_manager]:
+        raise exceptions.InsufficientPrivileges
+
+    batch: Optional[models.AccessoriesStorage] = (
+        db.query(models.AccessoriesStorage)
+            .filter(models.AccessoriesStorage.article == article)
+            .one_or_none()
+    )
+
+    accessory: models.Accessory = db.query(models.Accessory).filter(models.Accessory == article).one_or_none()
+
+    if accessory is None:
+        raise exceptions.ArticleDoesNotExist
+
+    if not accessory.kg_acceptable:
+        return JSONResponse(
+            status_code= 400,
+            content={
+                "detail": "Cannot decommission in kg on this item"
+            }
+        )
+
+    quantity = round(amount / accessory.weight)
+
     if batch is None:
         raise exceptions.InvalidClothStorage
 
